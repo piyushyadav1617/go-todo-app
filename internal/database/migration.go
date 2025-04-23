a96e8f647246ee639feac89709864036bec87c56
Piyush <yadavpiyush222@gmail.com>
2025-04-23 09:31:37 +0530

    adds user in db

--- internal/database/migration.go
+++ internal/database/migration.go
@@ -7,11 +7,22 @@ import (
 func Migrate() error {
 	db := GetDB()
 	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+        );
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to migrate database: %w", err)
+	}
+	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS todos (
             id SERIAL PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             description TEXT,
-            completed BOOLEAN NOT NULL DEFAULT false
+            completed BOOLEAN NOT NULL DEFAULT false,
+			user_id Int NOT NULL REFERENCES users(id)
         );
 	`)
 	if err != nil {
